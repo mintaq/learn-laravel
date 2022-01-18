1d510975407d64854d4760a867663c603835b8c8
mintaq <minh.tranquang98@gmail.com>
2022-01-18 22:49:20 +0700

    add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // DB::table('users')->insert([
+        //     'name' => 'Minh Tran',
+        //     'email' => 'test@omegatheme.com',
+        //     'email_verified_at' => now(),
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10),
+        // ]);
+
+        User::factory()->state([
+            'name' => 'Minh Tran',
+            'email' => 'test@omegatheme.com',
+        ])->create();
+        User::factory()->count(20)->create();
     }
 }
