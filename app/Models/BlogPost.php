34019520c63c7444afa7b6a78cf55b3632456fc7
mintaq <minh.tranquang98@gmail.com>
2022-01-23 09:42:43 +0700

    add soft delete

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['title', 'content'];
 
@@ -16,11 +17,15 @@ class BlogPost extends Model
         return $this->hasMany('App\Models\Comment');
     }
 
-    // public static function boot() {
-    //     parent::boot();
+    public static function boot() {
+        parent::boot();
 
-    //     static::deleting(function (BlogPost $blogPost) {
-    //         $blogPost->comments()->delete();
-    //     });
-    // }
+        static::deleting(function (BlogPost $blogPost) {
+            $blogPost->comments()->delete();
+        });
+
+        static::restoring(function (BlogPost $blogPost) {
+            $blogPost->comments()->restore();
+        });
+    }
 }
